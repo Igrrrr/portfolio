f0b91e2c68c5f2ffdde4ac1bf3e5240056d75388
Igor Konovodov <konovodov@list.ru>
2024-09-09 11:35:21 +0300

    first commit

---
+++ src/js/modules/data.js
@@ -0,0 +1,65 @@
+export const techStackIcons = [
+  { html: "html.svg" },
+  { css: "css.svg" },
+  { js: "js.svg" },
+  { react: "react.svg" },
+  { redux: "redux.svg" },
+  { bootstrap: "bootstrap.svg" },
+  { tailwind: "tailwind.svg" },
+  { sass: "sass.svg" },
+  { git: "git.svg" },
+  { greensock: "greensock.svg" },
+  { vsc: "vscode.svg" },
+  { github: "ghblack.svg" },
+];
+
+export const card = [
+  {
+    image: "Rectangle4.png",
+    alt: "Rectangle4",
+    title: "Project Tile goes here",
+    description:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    skills: "HTML , JavaScript, SASS, React",
+  },
+  {
+    image: "Rectangle6.png",
+    alt: "Rectangle6",
+    title: "Project Tile goes here",
+    description:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    skills: "HTML , JavaScript, SASS, React",
+  },
+  {
+    image: "Rectangle8.png",
+    alt: "Rectangle8",
+    title: "Project Tile goes here",
+    description:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    skills: "HTML , JavaScript, SASS, React",
+  },
+  {
+    image: "Rectangle12.png",
+    alt: "Rectangle12",
+    title: "Project Tile goes here",
+    description:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    skills: "HTML , JavaScript, SASS, React",
+  },
+  {
+    image: "Rectangle13.png",
+    alt: "Rectangle13",
+    title: "Project Tile goes here",
+    description:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    skills: "HTML , JavaScript, SASS, React",
+  },
+  {
+    image: "Rectangle14.png",
+    alt: "Rectangle14",
+    title: "Project Tile goes here",
+    description:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    skills: "HTML , JavaScript, SASS, React",
+  },
+];
