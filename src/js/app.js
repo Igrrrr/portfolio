7b140b02d6913d118ee9cebb2432218d694de984
Igor Konovodov <konovodov@list.ru>
2025-02-10 21:13:46 +0300

    add: slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,27 @@
+import Splide from "@splidejs/splide";
+
 const burger = document.getElementById("burger-toggle");
 const main = document.getElementById("main");
 const dialog = document.querySelector(".modal");
 const closeModal = document.querySelector(".modal__close-button");
 const cardContainer = document.querySelector(".projects__cards-container");
+
+//new Swiper(".swiper");
+//new Splide(".splide").mount();
+
+const slider = new Splide("#slider", {
+  //perPage: 1,
+  gap: "2rem",
+  perMove: "1",
+  breakpoints: {
+    640: {
+      perPage: 1,
+      padding: { top: "2rem", left: "1rem", right: "1rem" },
+      arrows: false,
+    },
+  },
+});
+slider.mount();
 
 const getSrc = (e) => {
   if (e.target.closest(".card")) {
